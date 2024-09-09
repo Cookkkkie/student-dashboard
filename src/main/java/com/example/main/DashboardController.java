89d28c48024ff0938acca25ab9e51b691d9066f1
Oleksii Pecheniuk <aleksey.pecheniuk@icloud.com>
2024-09-09 14:53:01 +0300

    Added Springboot

---
+++ src/main/java/com/example/main/DashboardController.java
@@ -0,0 +1,13 @@
+package com.example.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashboardController {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashboardController.class, args);
+	}
+
+}
