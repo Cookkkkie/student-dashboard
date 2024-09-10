8f0d25f91f395d4e734d52b4c4b96a47ddfad41f
Arnas Šniokaitis <sniokaitisarnas@gmail.com>
2024-09-10 17:42:46 +0300

    Added rudamentary implementation of getting the user by ID

--- src/main/java/com/example/main/UserController.java
+++ src/main/java/com/example/main/UserController.java
@@ -1,9 +1,22 @@
 package com.example.main;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("user/")
 public class UserController {
+    @Autowired UserService service;
+    @PostMapping("get/{id}")
+    public ResponseEntity<User> getAccountById(@PathVariable int id, @RequestParam String password){
+        User response = service.getByID(id);
+       if(response != null) {
+           return ResponseEntity.ok(response);
+       }
+        return ResponseEntity.notFound().build();
+    }
 
 }
+
--- src/main/java/com/example/main/UserService.java
+++ src/main/java/com/example/main/UserService.java
@@ -2,9 +2,18 @@ package com.example.main;
 
 import org.springframework.stereotype.Service;
 
+import java.sql.SQLException;
+
 @Service
 public class UserService {
 
 
+    public User getByID(int ID){
+        try {
+            return MySqlDatabase.getByID(ID);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 }
