fc22c961e091747c0e2dcd37eedba09a1943ab47
Arnas Šniokaitis <sniokaitisarnas@gmail.com>
2024-09-10 17:04:02 +0300

    Created husk for development of getting, putting and changing the data of users.

---
+++ src/main/java/com/example/main/User.java
@@ -0,0 +1,9 @@
+package com.example.main;
+
+public class User {
+    int userID;
+    String name;
+    String lastName;
+    String email;
+    String password;
+}
---
+++ src/main/java/com/example/main/UserService.java
@@ -0,0 +1,10 @@
+package com.example.main;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+
+
+}
