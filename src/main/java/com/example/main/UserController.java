ad1f5dd59ed5ba1c332208f63e0d4c033d86deb6
EPKerke <evelina.paula.kerke@gmail.com>
2024-09-10 17:48:18 +0300

    Added UserRepository and updated UserService and UserController

--- src/main/java/com/example/main/UserController.java
+++ src/main/java/com/example/main/UserController.java
@@ -1,9 +1,24 @@
 package com.example.main;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("user/")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/create")
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
+        User createdUser = userService.createUser(user);
+        return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+    }
 
 }
--- src/main/java/com/example/main/UserService.java
+++ src/main/java/com/example/main/UserService.java
@@ -1,10 +1,16 @@
 package com.example.main;
 
+import com.example.main.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
+    @Autowired
+    private UserRepository userRepository;
 
-
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
 }
