12f79810f5336aa9e8db86f49b0817fda140073f
Oleksii Pecheniuk <aleksey.pecheniuk@icloud.com>
2024-09-10 11:01:32 +0300

    Switched for a MySQL database

--- src/main/java/com/example/main/DATABASE.java
+++ src/main/java/com/example/main/DATABASE.java
@@ -1,47 +1,53 @@
 package com.example.main;
 
+import org.springframework.stereotype.Component;
+
 import java.sql.*;
 
+@Component
 public class DATABASE {
-    private static final String url = "jdbc:sqlite:database.db";
+    private static final String url = "jdbc:mysql://localhost:3306/dashboard?serverTimezone=UTC";
     private Connection conn;
-    public static void main(String[] args) throws SQLException {
 
-        Connection connection = DriverManager.getConnection(url);
-        Statement statement = connection.createStatement();
+    public static void main(String args[]) throws SQLException {
 
-        String createUsers = "CREATE TABLE IF NOT EXISTS users(" +
-                "    userID INTEGER PRIMARY KEY AUTOINCREMENT," +
-                "    name VARCHAR(100) NOT NULL," +
-                "    lastName VARCHAR(100) NOT NULL," +
-                "    email VARCHAR(255) NOT NULL UNIQUE," +
-                "    password VARCHAR(255) NOT NULL);";
+        Connection conn = DriverManager.getConnection(url, "oleksii", "1234");
+        Statement statement = conn.createStatement();
+        //CREATE DATABASE name;
+        String createUsers = "CREATE TABLE IF NOT EXISTS users (" +
+                "userID INT AUTO_INCREMENT PRIMARY KEY," +
+                "name VARCHAR(100) NOT NULL," +
+                "lastName VARCHAR(100) NOT NULL," +
+                "email VARCHAR(255) NOT NULL UNIQUE," +
+                "password VARCHAR(255) NOT NULL" +
+                ");";
 
-        String createCourse = "CREATE TABLE IF NOT EXISTS course(" +
-                "    courseID INTEGER PRIMARY KEY AUTOINCREMENT," +
-                "    name VARCHAR(100) NOT NULL," +
-                "    userID INT," +
-                "    FOREIGN KEY (userID) REFERENCES users(userID) ON DELETE CASCADE" +
+        String createCourse = "CREATE TABLE IF NOT EXISTS course (" +
+                "courseID INT AUTO_INCREMENT PRIMARY KEY," +
+                "name VARCHAR(100) NOT NULL," +
+                "userID INT," +
+                "FOREIGN KEY (userID) REFERENCES users(userID) ON DELETE CASCADE" +
                 ");";
 
         String createAssignments = "CREATE TABLE IF NOT EXISTS assignments (" +
-                "    assignmentID INTEGER PRIMARY KEY AUTOINCREMENT," +
-                "    name VARCHAR(100) NOT NULL," +
-                "    dueDate DATE NOT NULL," +
-                "    status BOOLEAN," +
-                "    courseID INT," +
-                "    FOREIGN KEY (courseID) REFERENCES course(courseID) ON DELETE CASCADE" +
+                "assignmentID INT AUTO_INCREMENT PRIMARY KEY," +
+                "name VARCHAR(100) NOT NULL," +
+                "dueDate DATE NOT NULL," +
+                "status BOOLEAN," +
+                "courseID INT," +
+                "FOREIGN KEY (courseID) REFERENCES course(courseID) ON DELETE CASCADE" +
                 ");";
 
         String createToDoList = "CREATE TABLE IF NOT EXISTS ToDoList (" +
-                "    listID INTEGER PRIMARY KEY AUTOINCREMENT," +
-                "    name VARCHAR(100) NOT NULL," +
-                "    content TEXT," +
-                "    status BOOLEAN," +
-                "    due_date DATE," +
-                "    userID INT," +
-                "    FOREIGN KEY (userID) REFERENCES users(userID) ON DELETE CASCADE" +
+                "listID INT AUTO_INCREMENT PRIMARY KEY," +
+                "name VARCHAR(100) NOT NULL," +
+                "content TEXT," +
+                "status BOOLEAN," +
+                "due_date DATE," +
+                "userID INT," +
+                "FOREIGN KEY (userID) REFERENCES users(userID) ON DELETE CASCADE" +
                 ");";
+
 
         statement.execute(createUsers);
         statement.execute(createCourse);
@@ -64,6 +70,10 @@ public class DATABASE {
         statement.executeUpdate();
     }
 
+    public void addToDo(String name, String content, boolean status, Date due_date){
+
+    }
+
     public void readUser(String email) throws SQLException {
         String readUser = "SELECT * FROM users WHERE email = ?;";
         conn = DriverManager.getConnection(url);
@@ -76,13 +86,37 @@ public class DATABASE {
 
     }
 
-    public void readCourse() {
+    public void readCourse(String courseID) throws SQLException {
+        String readCourse = "SELECT * FROM course WHERE courseID = ?;";
+        conn = DriverManager.getConnection(url);
+        PreparedStatement statement = conn.prepareStatement(readCourse);
+        statement.setString(1, courseID);
+        ResultSet resultSet = statement.executeQuery();
+        while (resultSet.next()) {
+            System.out.println(resultSet.getString("name") + " " + resultSet.getString("courseID"));
+        }
     }
 
-    public void readAssignment() {
+    public void readAssignment(String assignmentID) throws SQLException {
+        String readAssignment = "SELECT * FROM assignments WHERE assignmentID = ?;";
+        conn = DriverManager.getConnection(url);
+        PreparedStatement statement = conn.prepareStatement(readAssignment);
+        statement.setString(1, assignmentID);
+        ResultSet resultSet = statement.executeQuery();
+        while (resultSet.next()) {
+            System.out.println(resultSet.getString("name") + " " + resultSet.getString("courseID"));
+        }
     }
 
-    public void readToDo() {
+    public void readToDo(String listID) throws SQLException {
+        String readToDo = "SELECT * from ToDoList where listID = ?;";
+        conn = DriverManager.getConnection(url);
+        PreparedStatement statement = conn.prepareStatement(readToDo);
+        statement.setString(1, listID);
+        ResultSet resultSet = statement.executeQuery();
+        while (resultSet.next()) {
+            System.out.println(resultSet.getString("name") + " " + resultSet.getString("courseID"));
+        }
     }
 
 
--- src/main/java/com/example/main/DashboardController.java
+++ src/main/java/com/example/main/DashboardController.java
@@ -3,11 +3,14 @@ package com.example.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.SQLException;
+
 @SpringBootApplication
 public class DashboardController {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 		SpringApplication.run(DashboardController.class, args);
-	}
+		DATABASE.main(args);
+	}    
 
 }
