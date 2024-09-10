9e6242238652c1283e8fe89474f36c41f2249145
EPKerke <evelina.paula.kerke@gmail.com>
2024-09-10 18:09:47 +0300

    fixed users.java class

--- src/main/java/com/example/main/User.java
+++ src/main/java/com/example/main/User.java
@@ -1,7 +1,14 @@
 package com.example.main;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+
 public class User {
-    int userID;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int userID;
     String name;
     String lastName;
     String email;
