271943befc974a2cc85145dc989ab8b5bc29ac6a
Anjanee Sewvandika Wijewardana <anjaneew@gmail.com>
2024-09-11 08:51:22 +0300

    UserUpdateDto is added
    UserUpdateDto is added

---
+++ UserUpdateDto.java
@@ -0,0 +1,51 @@
+package dtos;
+
+public class UserUpdateDto {
+
+    private int userID;
+    private String name;
+    private String lastName;
+    private String email;
+    private String password;
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
+
