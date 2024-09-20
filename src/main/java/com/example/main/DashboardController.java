5358a4b29388ff5524ec404f372e8b56aa379863
Oleksii Pecheniuk <aleksey.pecheniuk@icloud.com>
2024-09-20 10:25:45 +0300

    Final bug fixing

--- src/main/java/com/example/main/DashboardController.java
+++ src/main/java/com/example/main/DashboardController.java
@@ -2,6 +2,7 @@ package com.example.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,7 +12,7 @@ import java.util.TimeZone;
 @SpringBootApplication
 @Controller
 @RequestMapping("/")
-public class DashboardController {
+public class DashboardController implements ErrorController {
 
 	@RequestMapping("/")
 	public static void main(String[] args) throws SQLException {
@@ -19,4 +20,9 @@ public class DashboardController {
 		SpringApplication.run(DashboardController.class, args);
 		MySqlDatabase.createTables();
 	}
+	@RequestMapping("/error")
+	public String redirect(){
+		return "redirect:/main/dashboard";
+	}
+
 }
